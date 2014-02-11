09801bb4f81a87ec199d5cbe85668c800b809155
hirokazu.iwashita <kazuhiro.iwashita@jibemobile.jp>
2014-02-12 02:59:09 +0900

    First Test

---
+++ FirstTest/FirstTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FirstTest
+//
+//  Created by Iwashita Hirokazu on 2014/02/12.
+//  Copyright (c) 2014年 RUDIO.K.K. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FirstTest/FirstTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FirstTest
+//
+//  Created by Iwashita Hirokazu on 2014/02/12.
+//  Copyright (c) 2014年 RUDIO.K.K. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
